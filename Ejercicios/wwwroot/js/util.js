c9c4171d89591d7380dbb9f8b86d10652fdf580c
JIsma <jismael-rn@outlook.com>
2022-03-06 17:16:16 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicios/wwwroot/js/util.js
@@ -0,0 +1,22 @@
+﻿function MostrarAlerta(msj) {
+    Swal.fire(msj)
+}
+
+function validaEntero(idIntpup, idSpan) {
+    var valor = document.getElementById(idIntpup).value;
+    document.getElementById(idIntpup).value = valor.trim();
+    var span = document.getElementById(idSpan);
+    var regex = new RegExp('^[0-9]+$');
+
+    if (!regex.test(valor)) {
+        span.innerHTML = 'Se requiere valor entero';
+        span.style.display = 'block';
+        return false;
+    }
+    else {
+        span.innerHTML = '';
+        span.style.display = 'none';
+        return true;
+    }
+
+}
